0e2ac9a5567e5d79f24b62e06ea68a43cff7cca1
Jared Stanley <jaredstanley@JAREDs-MacBook-Pro.local>
2013-05-21 00:44:03 -0700

    golden ratio

--- p11/js/script.js
+++ p11/js/script.js
@@ -1,91 +1,73 @@
-
-
 //
 var main_canvas,
 main_context,
-ang = 0, 
-freq = .08,
-vel = 0,
-stiffness = 1.04,
-dampening = .99,
-dir = 1;
+anglex = 0, 
+angley = 0, 
+freqx = .5,
+freqy = .5,
+amplitude = 0,
+radius = 2,
+curx = 0,
+cury = 0;
+var width;
 
-var v = [];  
-var posX = []; 
-var posY = []; 
-var n=300; 
-var width = 1000;
-var spacing = width/n;
-var waterlevel = 100;
-
-start();
+window.onload = function(){
+	start();
+}
 //
 function start(){
 	main_canvas = document.getElementById("c");
 	main_context = main_canvas.getContext("2d");
 
-	initWaves();
+	init();
 	initTimer();
 }
 
-function initWaves(){
-    for(var i=0; i < n; i++) {
-        v.push(0);
-    }
-    
- }
+function init(){
+	width = main_canvas.width;
+	main_context.fillStyle = '#BFffff';
+}
   
 function initTimer(){
 	window.requestAnimationFrame(initTimer);
 	update();
 }
 
-
 function update() {
-	main_context.clearRect(0,0,width,width);
+	//main_context.clearRect(0,0,width,width);
 	main_context.beginPath();
-	main_context.moveTo(0,width);
-	var tgt = Math.round(n*.8); // % across the surface the target point will be located
-	increaseVel();
-	
-	for(var i=0; i<n; i++){
-		posX[i] = spacing*i;
-		if(i==tgt){
-			//target
-			ang+=freq;
-			v[i] = Math.sin(ang)*vel;	
-		}else if(i<tgt){
-			//water
-			v[i] = (v[i+1]);
-		}else if(i>tgt){
-			//gelatin
-			v[i] = (v[i-1]/stiffness);
-		}
-		v[i]*=dampening;
-        posY[i] = v[i] + waterlevel;
-        //
-        main_context.lineTo(posX[i], posY[i]);	 
-posY
-	}
-	main_context.lineTo(width, width);
-	main_context.fillStyle = '#BFEFFF';
+	amplitude+=(.3);	
+	// amplitude=200;	
+	anglex+=freqx;
+	angley+=freqy;
+	curx = Math.cos(anglex)*(amplitude);
+	cury = Math.sin(angley)*(amplitude);
+	// cury = Math.tan(angley)*(amplitude);
+
+	curx+= width/2;
+	cury+= width/2;
+
+	main_context.arc(curx, cury, radius, 0, Math.PI*2, false);
+	main_context.fill();
+
+}
+function update2() {
+	//main_context.clearRect(0,0,width,width);
+	main_context.beginPath();
+	amplitude+=(freqx);	
+	// amplitude=200;	
+	anglex+=freqx;
+	angley+=freqy;
+	curx = Math.cos(anglex)*200;
+	cury = Math.sin(angley)*amplitude;
+
+	curx+= width/2;
+	cury+= width/2;
+
+	main_context.arc(curx, cury, radius, 0, Math.PI*2, false);
 	main_context.fill();
 
 }
 
-function increaseVel(){
-	
-	if(dir==1){
-		vel+=.2;
-		if(vel>100){
-			dir=0;
-		}
-	}else{
-		vel-=.8;
-		if(vel<0){
-			dir=1;
-		}
-	}
-}
 
 
