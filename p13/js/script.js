84e3c98b6215c16fe45c6200da9234e9ee6343e4
Jared Stanley <jared.stanley@gmail.com>
2013-06-07 18:18:37 -0700

    pushing

--- p13/js/script.js
+++ p13/js/script.js
@@ -8,12 +8,14 @@ var count = 0,
 x = 0, y=0, grad;
 
 var pointed = {
-	x: 200,
-	y: 200,
+	x: 0,
+	y: 0,
 	rad: 2,
 	ang:0,
-	inc:.005,
-	range:100
+	inc:.05,
+	range:100,
+	tgtX:0,
+	tgtY:0
 };
 
 var pointer = {
@@ -26,6 +28,7 @@ var pointer = {
 	rotation: 0
 };
 
+
 window.onload = function(){
 	start();
 }
@@ -37,10 +40,10 @@ function start(){
 	b_canvas = document.getElementById("c");
 	contxt = b_canvas.getContext("2d");
 
-	grad = contxt.createLinearGradient(0,0,100, b_canvas.width);
-	grad.addColorStop(0, "rgb(255, 222, 255)");
-	grad.addColorStop(1, "rgb(0, 0, 0)");
-	contxt.fillStyle = grad;
+	// grad = contxt.createLinearGradient(0,0,100, b_canvas.width);
+	// grad.addColorStop(0, "rgb(255, 222, 255)");
+	// grad.addColorStop(1, "rgb(0, 0, 0)");
+	// contxt.fillStyle = grad;
 	
 
 	var interval = setInterval(update,30);
@@ -71,33 +74,48 @@ function updatePointer(){
 
 	contxt.save();
 	contxt.translate(b_canvas.width/2, b_canvas.width/2);
-	// contxt.translate(pointer.x, pointer.y);
+	 //contxt.translate(pointer.x, pointer.y);
 	contxt.rotate(pointer.radians);
 	contxt.fillStyle = grad;
-	contxt.fillRect(-50, -5, 100, 10);
+	contxt.fillRect(0, 0, 50, 1);
 	contxt.restore();
- 	countIt(pointer.radians);
+ 	countIt(count);
 }
 
-function updatePointed(){
-	pointed.ang+=pointed.inc;
-	var npX = -100;
-	// var npX = Math.cos(pointed.ang)*pointed.range;
-	var npY = Math.sin(pointed.ang/.55)*pointed.range;
+// function updatePointed(){
+// 	pointed.ang+=pointed.inc;
+// 	var npX = -100;
+// 	// var npX = Math.cos(pointed.ang)*pointed.range;
+// 	var npY = Math.sin(pointed.ang/.55)*pointed.range;
 	
-	pointed.x = npX+(b_canvas.width/2);
-	pointed.y = npY+(b_canvas.width/2);
+// 	pointed.x = npX+(b_canvas.width/2);
+// 	pointed.y = npY+(b_canvas.width/2);
+
+// 	contxt.beginPath();
+// 	contxt.arc(pointed.x, pointed.y, pointed.rad, 0, Math.PI*2);
+// 	contxt.fill();
+// }
+
+
+function updatePointed(){
+	pointed.x+=(pointed.tgtX-pointed.x)/20;
+	pointed.y+=(pointed.tgtY-pointed.y)/20;
 
 	contxt.beginPath();
-	contxt.arc(pointed.x, pointed.y, pointed.rad, 0, Math.PI*2);
+	contxt.arc(pointed.x, pointed.y, 4, 0, Math.PI*2);
 	contxt.fill();
+
+	if((Math.abs(pointed.x-pointed.tgtX)<1) || (Math.abs(pointed.y-pointed.tgtY)<1)){
+		pointed.tgtX = Math.random()*500;
+		pointed.tgtY = Math.random()*500;
+	}
 }
 
 function countIt(num){
   
   contxt.fillStyle="#666666";
   contxt.font="30px sans-serif";
-  contxt.fillText(":"+num+":", 100, 100);
+  contxt.fillText(":"+(num|0)+":", 100, 100);
 
 }
 
